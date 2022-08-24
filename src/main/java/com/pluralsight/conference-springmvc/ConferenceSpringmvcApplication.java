55a22e6580e039108caebcc7bfff21cb07f9362a
MatthewEarlywine <78188324+MatthewEarlywine@users.noreply.github.com>
2022-08-24 14:14:51 -0500

    Add files via upload

---
+++ src/main/java/com/pluralsight/conference-springmvc/ConferenceSpringmvcApplication.java
@@ -0,0 +1,13 @@
+package com.pluralsight.conferencespringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceSpringmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceSpringmvcApplication.class, args);
+	}
+
+}
